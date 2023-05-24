c3ad8670f8e0f5339e504d7a3f184de9c5c33043
NidaBoy <daniboytorres99xx@gmail.com>
2023-05-24 15:08:15 -0300

    login feito

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,12 +1,25 @@
 import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 export const NavBar = () => {
+
+    const [cookies, setCookies] = useCookies(["access_token"]);
+    const Navigate = useNavigate();
+
+    const logout = () => {
+        setCookies("access_token", "");
+        window.localStorage.removeItem("userID");
+        Navigate("/conta")
+    };
+
     return(
         <div className="navbar">
             <Link to="/"> Inicial</Link>
             <Link to="/criar-receita"> Criar Receita</Link>
             <Link to="/receitas-salvas"> Receitas Salvas</Link>
-            <Link to="/conta"> Login/Cadastro</Link>
+            {!cookies.access_token ? (<Link to="/conta"> Login/Cadastro</Link>) 
+            : (<button onClick={logout}> Logout </button>)}
         </div>
     );
 };
--- client/src/pages/conta.js
+++ client/src/pages/conta.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
-
+import { useCookies} from "react-cookie";
+import { useNavigate } from "react-router-dom";
 export const Conta = () => {
     return (
         <div className="conta">
@@ -14,6 +15,24 @@ const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    const [_, setCookies] = useCookies(["access_token"]);
+
+    const navigate = useNavigate();
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:3001/conta/login', { username, password });
+
+            setCookies("access_token", response.data.token);
+            window.localStorage.setItem("userID", response.data.userID);
+            navigate("/");
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     return (
         <Form 
         username={username}
@@ -21,6 +40,7 @@ const Login = () => {
         password={password}
         setPassword={setPassword}
         label="Login"
+        onSubmit={onSubmit}
         />
     );
 };
