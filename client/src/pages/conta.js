e554a55a23e25a178c9cf89f7cd4c625e7455161
NidaBoy <daniboytorres99xx@gmail.com>
2023-05-24 12:10:05 -0300

    Configurando a página de login/cadastro e finalizando a funcao de cadastro

--- client/src/pages/conta.js
+++ client/src/pages/conta.js
@@ -1,3 +1,80 @@
+import { useState } from "react";
+import axios from "axios";
+
 export const Conta = () => {
-    return <div> Conta </div>;
+    return (
+        <div className="conta">
+            <Login />
+            <Cadastro />
+        </div>
+    );
+};
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return (
+        <Form 
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+        label="Login"
+        />
+    );
+};
+
+const Cadastro = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            await axios.post('http://localhost:3001/conta/cadastro', { username, password });
+            alert("Cadastro completado, faça o Login.");
+        }catch (err){
+            console.error(err);
+        }
+    };
+
+    return (
+        <Form 
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+        label="Cadastro"
+        onSubmit={onSubmit}
+        />
+    );
+};
+
+const Form = ({username, setUsername, password, setPassword, label, onSubmit}) => {
+    return (
+        <div className="conta-container">
+            <form onSubmit={onSubmit}>
+                <h2> {label} </h2>
+                <div className="form-grupo">
+                    <label htmlFor="username"> Usuário: </label>
+                    <input 
+                    type="text" 
+                    id="username"
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}/>
+                </div>
+                <div className="form-grupo">
+                    <label htmlFor="password"> Senha: </label>
+                    <input 
+                    type="password" 
+                    id="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}/>
+                </div>
+                <button type="submit"> {label}</button>
+            </form>
+        </div>
+    );
 };
